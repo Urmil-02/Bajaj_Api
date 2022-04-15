85a8d9bcd4f195fdb851566b290bab7a9099b57e
Urmil-02 <67201939+Urmil-02@users.noreply.github.com>
2022-04-15 13:14:05 +0530

    Add files via upload

---
+++ Bajaj_api.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+
+let port = 3000;
+const app = express()
+app.use(bodyParser.json());
+app.listen(port, () => {
+    console.log(`App running on ${port}, http://localhost:${port}`)
+})
+
+app.get('/',(req, res) => {
+    res.status(200).json({
+        "status": "working"
+    })
+})
+
+app.post("/bfhl", (req, res) => {
+    console.log(req.body.data)
+    let arra = req.body.data
+    let num = [];
+    let alphabet =[];
+    let user_id = "john_doe_17091999"
+    let email="john@xyz.com"
+    let rollNo = "0ABCD123"
+    let stat=false
+    arra.forEach((ele) => {
+        if(typeof(ele)==="number"){
+            console.log(`${ele} is number`)
+            num.push(ele)
+            stat=true
+        } else {
+            console.log(`${ele} is alphabet`)
+            alphabet.push(ele)
+            stat=true
+        }
+        
+
+    })
+    if(stat){
+        res.status(200).json({
+            "is_success": stat,
+            "user_id": user_id,
+            "email": email,
+            "roll_number":rollNo,
+            "numbers": num,
+            "alphabets": alphabet
+        })
+    } else {
+        res.status(200).json({
+            "is_success": stat,
+            "user_id": user_id
+        })
+
+    }
+})
